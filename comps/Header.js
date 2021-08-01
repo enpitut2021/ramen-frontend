acf87f8e5886ad6ae8328245ba75b13ce15feac7
nope0124 <kami-ryo0124@ezweb.ne.jp>
2021-08-01 19:09:40 +0900

    Fixed

--- comps/Header.js
+++ comps/Header.js
@@ -1,18 +1,25 @@
 import styles from '../styles/Home.module.css'
-
+import 'bootstrap/dist/css/bootstrap.min.css';
 const Header = () => {
     return ( 
         <div>
             <header>
-                <h1 class={styles.title}>タイトル</h1>
-                <nav class={styles.nav}>
-                <ul>
-                    <li class={styles.menu}><a href="#">項目1</a></li>
-                    <li class={styles.menu}><a href="#">項目1</a></li>
-                    <li class={styles.menu}><a href="#">項目1</a></li>
-                </ul>
+                <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+                    <div class="container">
+                    <a class="navbar-brand" href="/"><span>RAMEN</span></a>
+                    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+                        <span class="navbar-toggler-icon"></span>
+                    </button>
+                    <div class="collapse navbar-collapse" id="navbarNavDropdown">
+                        <ul class="navbar-nav ml-auto">
+                            <li><a class="fas fa-home nav-link text-light" href="/"> Home</a></li>
+                            <li><a class="fas fa-link nav-link text-light" href="/about"> Send</a></li>
+                            <li><a class="fas fa-link nav-link text-light" href="/about"> Receive</a></li>
+                        </ul>
+                    </div>
+                    </div>
                 </nav>
-            </header>
+                </header>
         </div>
     );
 }
--- pages/about.js
+++ pages/about.js
@@ -1,14 +1,17 @@
 import styles from '../styles/Home.module.css'
-
+import Header from "../comps/Header.js"
 
 const About = () => {
     return ( 
+        <div>
+            <Header />
         <div className={styles.container}>
             
             <div className={styles.box}>
                 <h1>残高</h1>
                 <h1>12.34 Eth</h1>
             </div>
+        </div>
         </div>
      );
 }
